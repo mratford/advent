f1040aeb804318adc0b2849234de618aa5cd95e8
Mike Ratford <michael.ratford@digital.justice.gov.uk>
2022-12-04 17:47:59 +0000

    Better parsing

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -1,15 +1,12 @@
+#[macro_use]
+extern crate scan_fmt;
+
 fn main() {
     let lines: Vec<&str> = include_str!("../input").trim().split('\n').collect();
     let mut total = 0;
     let mut total_2 = 0;
     for line in &lines {
-        let mut ranges = line.split(',');
-        let mut r1 = ranges.next().unwrap().split('-');
-        let l1 = r1.next().unwrap().parse::<i32>().unwrap();
-        let u1 = r1.next().unwrap().parse::<i32>().unwrap();
-        let mut r2 = ranges.next().unwrap().split('-');
-        let l2 = r2.next().unwrap().parse::<i32>().unwrap();
-        let u2 = r2.next().unwrap().parse::<i32>().unwrap();
+        let (l1, u1, l2, u2) = scan_fmt!(line, "{d}-{d},{d}-{d}", i32, i32, i32, i32).unwrap();
         if (l1 <= l2 && u1 >= u2) || (l2 <= l1 && u2 >= u1) {
             total += 1;
         }
