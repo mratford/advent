071eb56353db0a8a866d504184654b8f8b332c06
Mike Ratford <michael.ratford@digital.justice.gov.uk>
2022-12-09 17:13:44 +0000

    day 9

--- 2022/day09/day09.py
+++ 2022/day09/day09.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Callable, List, Tuple
 import pprint
 
 
@@ -11,11 +10,32 @@ class Point:
     def __add__(self, other):
         return self.__class__(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return self.__class__(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return self.__class__(-self.x, -self.y)
+
     def __hash__(self):
         return hash((self.x, self.y))
 
+    def unit(self):
+        if self.x < 0:
+            unit_x = -1
+        elif self.x == 0:
+            unit_x = 0
+        else:
+            unit_x = 1
+        if self.y < 0:
+            unit_y = -1
+        elif self.y == 0:
+            unit_y = 0
+        else:
+            unit_y = 1
+        return self.__class__(unit_x, unit_y)
 
-def move_closure() -> Callable[[Point, str], Point]:
+
+def move_closure():
     directions = {
         "R": Point(1, 0),
         "L": Point(-1, 0),
@@ -23,7 +43,7 @@ def move_closure() -> Callable[[Point, str], Point]:
         "D": Point(0, -1),
     }
 
-    def f(p: Point, d: str) -> Point:
+    def f(p, d):
         return p + directions[d]
 
     return f
@@ -32,17 +52,25 @@ def move_closure() -> Callable[[Point, str], Point]:
 move = move_closure()
 
 
-def distance(p1: Point, p2: Point) -> int:
+def distance(p1, p2):
     return max(abs(p1.x - p2.x), abs(p1.y - p2.y))
 
 
-def move_head_tail(head: Point, tail: Point, direction: str) -> Tuple[Point, Point]:
+def move_towards_head(head, tail):
+    return tail + (head - tail).unit()
+
+
+def move_tail_if_necessary(head, tail):
+    return tail if distance(head, tail) < 2 else move_towards_head(head, tail)
+
+
+def move_head_tail(head, tail, direction):
     new_head = move(head, direction)
     # Move tail into old head position if too far away
-    return (new_head, tail if distance(new_head, tail) < 2 else head)
+    return (new_head, move_tail_if_necessary(head, tail))
 
 
-def parse_data(s: str) -> List[Tuple[str, int]]:
+def parse_data(s):
     return [(sp[0], int(sp[1])) for sp in [x.split() for x in s.strip().splitlines()]]
 
 
@@ -60,32 +88,23 @@ def part_1(moves):
 
 
 def part_2(moves):
-    pp = pprint.PrettyPrinter(indent=4)
+    # pp = pprint.PrettyPrinter(indent=4)
     snake = [Point(0, 0) for _ in range(10)]
     nines = {snake[-1]}
 
     for d, n in moves:
         for _ in range(n):
-            new_snake = [None] * 10
-            new_snake[0], new_snake[1] = move_head_tail(snake[0], snake[1], d)
+            snake[0] = move(snake[0], d)
             for i in range(9):
-                new_snake[i + 1] = (
-                    snake[i + 1]
-                    if distance(snake[i + 1], new_snake[i]) < 2
-                    else snake[i]
-                )
-                if i != 8:
-                    if distance(new_snake[i + 1], snake[i + 2]) > 1:
-                        new_snake[i + 1] = move(snake[i + 1], d)
-            snake = new_snake
+                snake[i + 1] = move_tail_if_necessary(snake[i], snake[i + 1])
             nines.add(snake[-1])
-        print(d, n)
-        pp.pprint(dict(enumerate(snake)))
+        # print(d, n)
+        # pp.pprint(dict(enumerate(snake)))
 
     return len(nines)
 
 
 if __name__ == "__main__":
-    moves = parse_data(open("test_input_2").read())
+    moves = parse_data(open("input").read())
     print("Part 1:", part_1(moves))
     print("Part 2:", part_2(moves))
