37156639ee7e29b31b0d45152bc94d30cee3d155
Mike Ratford <michael.ratford@digital.justice.gov.uk>
2022-12-13 09:57:53 +0000

    Day 13

--- 2022/day13/day13.py
+++ 2022/day13/day13.py
@@ -1,4 +1,7 @@
-from itertools import count
+from itertools import count, chain
+from enum import Enum
+
+InOrder = Enum("InOrder", ["FALSE", "UNDECIDED", "TRUE"])
 
 
 def parse_data(s):
@@ -9,13 +12,57 @@ def parse_data(s):
 
 
 def right_order(left, right):
-    pass
+    match left, right:
+        case int(), list():
+            return right_order([left], right)
+        case list(), int():
+            return right_order(left, [right])
+        case int(), int():
+            return (
+                InOrder.FALSE
+                if left > right
+                else InOrder.UNDECIDED
+                if left == right
+                else InOrder.TRUE
+            )
+        case list(), list():
+            for l, r in zip(left, right):
+                match right_order(l, r):
+                    case InOrder.FALSE:
+                        return InOrder.FALSE
+                    case InOrder.TRUE:
+                        return InOrder.TRUE
+            if len(left) > len(right):
+                return InOrder.FALSE
+            elif len(right) > len(left):
+                return InOrder.TRUE
+            return InOrder.UNDECIDED
 
 
 def part_1(data):
-    return sum(i if right_order(*p) else 0 for i, p in zip(count(1), data))
+    # for p1, p2 in data:
+    #     print(p1)
+    #     print(p2)
+    #     print(right_order(p1, p2))
+    return sum(
+        i if right_order(*p) == InOrder.TRUE else 0 for i, p in zip(count(1), data)
+    )
+
+
+def part_2(data):
+    flattened_data = list(chain(*data))
+    index_2 = (
+        sum(1 if right_order(p, [[2]]) == InOrder.TRUE else 0 for p in flattened_data)
+        + 1
+    )
+    index_6 = (
+        sum(1 if right_order(p, [[6]]) == InOrder.TRUE else 0 for p in flattened_data)
+        + 2
+    )
+    return index_2 * index_6
 
 
 if __name__ == "__main__":
     data = parse_data(open("input").read())
     print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
