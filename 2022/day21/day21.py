16e1040e457a82dd7b3ddbddae777b73fd79f1a3
Mike Ratford <mike.ratfordf@gmail.com>
2022-12-23 06:06:34 +0000

    wip

--- 2022/day21/day21.py
+++ 2022/day21/day21.py
@@ -4,7 +4,7 @@ from fractions import Fraction
 dag = graphlib.TopologicalSorter()
 values = {}
 
-for line in open("test_input").read().strip().splitlines():
+for line in open("input").read().strip().splitlines():
     a, bs = line.split(": ")
     bs = bs.split()
     if len(bs) == 1:
@@ -17,8 +17,6 @@ for line in open("test_input").read().strip().splitlines():
 for x in dag.static_order():
     values[x] = eval(values[x])
 
-print(values[x])
-
 values = {}
 dag = graphlib.TopologicalSorter()
 
@@ -27,7 +25,7 @@ for line in open("input").read().strip().splitlines():
     if a != "humn":
         bs = bs.split()
         if len(bs) == 1:
-            values[a] = f"Fraction({bs[0]})"
+            values[a] = bs[0]
             dag.add(a)
         else:
             dag.add(a, bs[0], bs[2])
@@ -40,13 +38,16 @@ for n in dag.static_order():
         xs = values[n].split()
         if len(xs) > 1:
             x, op, y = xs
-            if x != "humn" and "humn" not in x:
-                x = eval(x).__repr__()[1:-1]
-            if y != "humn" and "humn" not in y:
-                y = eval(y).__repr__()[1:-1]
+            print(x)
+            print(op)
+            print(y)
+            print()
+            if "humn" not in x:
+                x = str(eval(x))
+            if "humn" not in y:
+                y = str(eval(y))
             if "humn" not in x and "humn" not in y:
-                print(f"{x} {op} {y}")
-                values[n] = eval(f"{x} {op} {y}").__repr__()[1:-1]
+                values[n] = eval(f"{x} {op} {y}")
             else:
                 values[n] = f"({x} {op} {y})"
 
@@ -55,12 +56,12 @@ root = Fraction(root)
 eq = eq.replace("values['humn']", "humn")
 while eq.startswith("("):
     eq = eq[1:-1]
-    print(eq)
-    # print(root, "=", eq)
+    root = Fraction(root)
     if eq.startswith("("):
         xs = eq.split(" ")
-        x = int(xs[-1])
+        x = Fraction(xs[-1])
         op = xs[-2]
+        print(f"X {op} {x} = {root}")
         if op == "*":
             root = root / x
         elif op == "/":
@@ -72,8 +73,9 @@ while eq.startswith("("):
         eq = " ".join(xs[:-2])
     elif eq.endswith(")"):
         xs = eq.split(" ")
-        x = int(xs[0])
+        x = Fraction(xs[0])
         op = xs[1]
+        print(f"{x} {op} X = {root}")
         if op == "*":
             root = root / x
         elif op == "/":
