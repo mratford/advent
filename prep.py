fae97367981d3d228311f37d28b8d73f5feabd62
Mike Ratford <mike.ratford@gmail.com>
2024-11-30 06:29:45 +0000

    2024 prep

--- prep.py
+++ prep.py
@@ -1,57 +1,19 @@
-import os
 from pathlib import Path
 
-YEAR = "2023"
+import nbformat
 
-nb_template = """{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "id": "268eeed3-4573-4b1d-9cb6-43d41b160b24",
-   "metadata": {},
-   "source": [
-    "# Day x \\n",
-    "## Part 1"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "494f7b41-a035-4dd5-bf3d-79a642bc558b",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (AoC)",
-   "language": "python",
-   "name": "advent"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}"""
-
-
-os.makedirs(YEAR, exist_ok=True)
+YEAR = "2024"
 
 for day in range(1, 26):
     zeroday = f"day{day:02}"
-    os.makedirs(os.path.join(YEAR, zeroday), exist_ok=True)
+    d = Path(YEAR) / zeroday
+    d.mkdir(parents = True, exist_ok=True)
+    
+    nb = nbformat.v4.new_notebook()
+    nb["cells"] = [
+        nbformat.v4.new_markdown_cell(f"# Day {day}\n## Part 1\n"),
+        nbformat.v4.new_code_cell()
+    ]
 
-    with open(os.path.join(YEAR, zeroday, zeroday + ".ipynb"), "w") as f:
-        f.write(nb_template.replace("Day x", f"Day {day}"))
+    with open(Path(YEAR) / zeroday / f"{zeroday}.ipynb", "w") as f:
+        nbformat.write(nb, f)
