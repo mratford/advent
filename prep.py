69b82ba6b34227f2649c027b618033e1bed7fcbb
Mike Ratford <mike.ratford@gmail.com>
2025-11-30 10:17:47 +0000

    2025 prep

--- prep.py
+++ prep.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 import nbformat
 
-YEAR = "2024"
+YEAR = "2025"
 
-for day in range(1, 26):
+for day in range(1, 13):
     zeroday = f"day{day:02}"
     d = Path(YEAR) / zeroday
-    d.mkdir(parents = True, exist_ok=True)
-    
+    d.mkdir(parents=True, exist_ok=True)
+
     nb = nbformat.v4.new_notebook()
     nb["cells"] = [
         nbformat.v4.new_markdown_cell(f"# Day {day}\n## Part 1\n"),
-        nbformat.v4.new_code_cell()
+        nbformat.v4.new_code_cell(),
     ]
 
     with open(Path(YEAR) / zeroday / f"{zeroday}.ipynb", "w") as f:
